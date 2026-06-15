49f3de807f4de5d4351bde66d0c6a98484f53b1d
pinjeti <pinjeti@jaguarlandrover.com>
2026-06-15 21:55:13 +0530

    My first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    # Binding to 0.0.0.0 ensures the cloud provider can route traffic to it
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
